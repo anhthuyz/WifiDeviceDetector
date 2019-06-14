3798b2b7624e84c1874da11b825cfbf2b274a898
Cao Anh Thuy <42934193+anhthuyz@users.noreply.github.com>
2019-06-14 09:32:33 +0700

    Update dc.js
    Change check command to find if device in list already.

--- src/dc.js
+++ src/dc.js
@@ -2,7 +2,7 @@
 
 var macList = []
 var exec = require('child_process').exec;
-var scanProcess = exec('tshark -l -Ini mon0 -s 256 type mgt subtype probe-req');
+var scanProcess = exec('tshark -l -Ini en0 -s 256 type mgt subtype probe-req');
 
 scanProcess.stdout.on('data', function(data) {
     console.log(data); 
@@ -10,10 +10,13 @@ scanProcess.stdout.on('data', function(data) {
         lastNumberOfDevice = macList.length; 
         let tempArray = data.split(/\s+/);
         let currentMacAddress = tempArray[3];
-        if (!macList.indexOf(currentMacAddress) > -1) {
+        if (!macList.includes(currentMacAddress)) {
             macList.push(currentMacAddress);
         }
         newNumberOfDevice = macList.length;
-        console.log('Current Wifi client device in range: ' + newNumberOfDevice);
+        if (newNumberOfDevice != lastNumberOfDevice) {
+            console.log('Wifi client device in range: ' + newNumberOfDevice);
+            // console.log(macList.toString());
+        }
     }
 });
